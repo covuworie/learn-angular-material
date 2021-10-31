e00fde0445e405f4853077914679518f7a3dd1e4
covuworie <chuk.ovuworie@gmail.com>
2021-10-31 15:15:37 -0500

    05 - Button Toggle

---
+++ src/05-Button-Toggle/material-demo/src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+const MaterialComponents = [MatButtonModule, MatButtonToggleModule];
+
+@NgModule({
+  exports: [MaterialComponents],
+})
+export class MaterialModule {}
