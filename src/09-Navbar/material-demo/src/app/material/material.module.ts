0d7e7f5f8b9384b846f0e9e7d082573e561ae774
covuworie <chuk.ovuworie@gmail.com>
2021-11-04 16:36:10 -0500

    09 - Navbar

---
+++ src/09-Navbar/material-demo/src/app/material/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const material = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+];
+
+@NgModule({
+  exports: [material],
+})
+export class MaterialModule {}
